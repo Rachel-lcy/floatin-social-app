aa3acbaa14de66b4f64270a1c836bb1f8f3c28a7
Rachel <hurui.lcy@gmail.com>
2025-07-24 21:03:10 -0600

    improve index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,4 +1,21 @@
-// pages/index.tsx
-export default function Home() {
-  return <h1>Welcome to Floatin Social App</h1>;
+import { builder, BuilderComponent } from "@builder.io/react";
+
+builder.init(process.env.BUILDER_PUBLIC_KEY);
+
+export async function getStaticProps() {
+  const page = await builder
+    .get("page", { url: "/" })
+    .toPromise();
+
+  return {
+    props: {
+      page: page || null,
+    },
+    revalidate: 5,
+  };
+}
+
+export default function HomePage({ page }) {
+  if (!page) return <div>404 - Page not found</div>;
+  return <BuilderComponent model="page" content={page} />;
 }
