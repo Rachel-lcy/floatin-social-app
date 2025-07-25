01da23a61faf58ec635bf0935ec0cd4d46719c90
Rachel <hurui.lcy@gmail.com>
2025-07-24 21:08:44 -0600

    fail to connect to Builder.io

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,21 +1,24 @@
 import { builder, BuilderComponent } from "@builder.io/react";
 
-builder.init(process.env.BUILDER_PUBLIC_KEY);
+builder.init(process.env.NEXT_PUBLIC_BUILDER_PUBLIC_KEY);
 
 export async function getStaticProps() {
-  const page = await builder
-    .get("page", { url: "/" })
-    .toPromise();
-
+  const page = await builder.get("page", { url: "/" }).promise();
   return {
     props: {
       page: page || null,
     },
-    revalidate: 5,
   };
 }
 
-export default function HomePage({ page }) {
-  if (!page) return <div>404 - Page not found</div>;
-  return <BuilderComponent model="page" content={page} />;
+export default function Home({ page }) {
+  return (
+    <>
+      {page ? (
+        <BuilderComponent model="page" content={page} />
+      ) : (
+        <h1>404 - Page Not Found</h1>
+      )}
+    </>
+  );
 }
