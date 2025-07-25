eae81e6dd8f1f4acc52558145fb94616d16f8ebc
Rachel <hurui.lcy@gmail.com>
2025-07-24 20:48:34 -0600

    fix params?.page and join

--- pages/[...page].tsx
+++ pages/[...page].tsx
@@ -5,7 +5,13 @@ builder.init(process.env.BUILDER_PUBLIC_KEY!);
 console.log(process.env.BUILDER_PUBLIC_KEY);
 
 export async function getStaticProps({ params }: GetStaticPropsContext) {
-  const urlPath = "/" + (params?.page?.join("/") || "");
+  let urlPath = "/";
+  if (Array.isArray(params?.page)) {
+    urlPath += params.page.join("/");
+  } else if (typeof params?.page === "string") {
+    urlPath += params.page;
+  }
+
   const page = await builder.get("page", { url: urlPath }).promise();
 
   return { props: { page } };
